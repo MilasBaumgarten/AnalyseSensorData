86b0d602c99854a107c6b210c792dc4487b952e9
Milas Baumgarten <milas.baumgarten@freenet.de>
2019-05-12 15:02:45 +0200

    merge PrepareData.py and Data.py, add comments

--- AnalyseSensorData/Data.py
+++ AnalyseSensorData/Data.py
@@ -2,12 +2,26 @@ import numpy as np
 import sys
 
 class Data(object):
-	def __init__(self):
-		print("created")
+	x_full = y_full = x = y_ecg = y_ecg_normalized = y_ecg_delta = y_eda = np.array([])
+	beat_delay = []
+
+	def __init__(self, tracking_data_file, sensor_data_file, min_delta, min_val, max_val, min_heart_beat_delay, max_heart_beat_delay, average_heart_beat_delay):
+		# get raw data and separate into specific arrays
+		self.x_full, self.y_full = self.extract(tracking_data_file, sensor_data_file)
+		self.x = np.squeeze(np.asarray(self.y_full[:,np.array([True, False, False, False])]))
+		self.y_ecg = np.squeeze(np.asarray(self.y_full[:,np.array([False, False, True, False])]))
+		self.y_eda = np.squeeze(np.asarray(self.y_full[:,np.array([False, True, False, False])]))
+		
+		# calculate gradient and normalize ecg data
+		self.y_ecg_delta = self.calc_gradient(self.y_ecg, True)
+		self.y_ecg_normalized = self.normalize(self.y_ecg_delta, min_delta, min_val, max_val)
+
+		# try to fix measurment errors by inserting missed heartbeats and deleting duplicate heart beats
+		for i in range(0, 1):
+			self.calculate_missed_heart_beats(min_val, max_val, min_heart_beat_delay, max_heart_beat_delay, average_heart_beat_delay)
 
 	# Source: "The Best Way to Prepare a Dataset Easily" - Siraj Raval - https://www.youtube.com/watch?v=0xVqLJe9_CY&t=437s
-	@staticmethod
-	def extract(tracking_filename, sensor_filename):
+	def extract(self, tracking_filename, sensor_filename):
 		# arrays to hod the labels and the feature vectors
 		labels = []
 		fvecs = []
@@ -47,23 +61,53 @@ class Data(object):
 		#return a pair of the feature matrix and the label matrix
 		return fvecs_np, labels_np
 
-	@staticmethod
-	def generate_dummy_data():
-		x_train = []	# input
-		y_train = []	# target output
-		for i in range(0, 1000):
-			x_train.append(np.random.random())
-			y_train.append(np.sin(x_train[i]))
+	# calculate gradient
+	def calc_gradient(self, array, cut):
+		delta = 0
+		last = 0
+		gradient = []
+		for i in array:
+			delta = i - last
+			last = i
+			if (cut and delta < 0):
+				delta = 0
+			gradient.append(np.abs(delta))
+		return gradient
 
-		x_train = np.array(x_train)
-		y_train = np.array(y_train)
+	# normalize values
+	def normalize(self, delta, min_delta, min_val, max_val):
+		last = 0
+		normalized = []
+		for i in delta:
+			if ((np.sign(last) != np.sign(i)) and np.abs(i) >= min_delta):
+				normalized.append(max_val)
+			else:
+				normalized.append(min_val)
+			last = i
+		return normalized
 
-		# generate dummy data for final test
-		x_test = []
-		y_test = []
-
-		for i in range(0, 100):
-			x_test.append(np.random.random())
-			y_test.append(np.sin(x_test[i]))
-
-		return x_train, y_train, x_test, y_test
+	# calculate time between heart (suspected) beats
+	def calculate_missed_heart_beats(self, min_val, max_val, min_delay, max_delay, average_delay):
+		self.beat_delay.clear()
+		last = 0
+		for i in range(0, len(self.y_ecg_normalized)):
+			# start check when a heart beat was found
+			if (self.y_ecg_normalized[i] == max_val):
+				index_delta = (i - last)
+				# remove a heart beat if it seems to be too fast aka duplicate
+				if (index_delta < min_delay):
+					# check which gradient was higher
+					if (self.y_ecg_delta[last] < self.y_ecg_delta[i]):
+						self.y_ecg_normalized[last] = min_val
+					else:
+						self.y_ecg_normalized[i] = min_val
+				else :
+					# insert missed heart beats
+					if (index_delta > max_delay):
+						missed_beats = index_delta // average_delay
+						for j in range(1, missed_beats):
+							self.y_ecg_normalized[last + int(np.floor((j / missed_beats) * index_delta))] = max_val
+							self.beat_delay.append(int(np.floor((1 / missed_beats) * index_delta)))
+					else:
+						self.beat_delay.append(index_delta)
+					last = i
--- AnalyseSensorData/TestStuff.py
+++ AnalyseSensorData/TestStuff.py
@@ -1,16 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from Data import Data
-from PrepareData import PrepareData
 
-min_heart_beat_delay = 15
-max_heart_beat_delay = 45
+min_heart_beat_delay = 20
+max_heart_beat_delay = 35
 average_heart_beate_delay = 25
 min_normalized_value = 0
 max_normalized_value = 500
-min_beat_delta = 50
+min_beat_delta = 40
 
-data = PrepareData( "TrackingData_2019-05-09-19-13-42.txt",
+data = Data( "TrackingData_2019-05-09-19-13-42.txt",
 					"SensorData_2019-05-09-19-13-42.txt",
 					min_beat_delta,
 					min_normalized_value,
@@ -28,8 +27,13 @@ for i in range (0, len(data.y_ecg)):
 	combined.append([data.y_ecg[i], data.y_ecg_normalized[i]])
 
 # draw graph
+plt.figure(1)
+plt.subplot(211)
 plt.ylabel("ecg")
 plt.plot(data.x, combined)
-#plt.plot(data.beat_delay)
+
+plt.subplot(212)
+plt.ylabel("delay")
+plt.plot(data.beat_delay)
 
 plt.show()
