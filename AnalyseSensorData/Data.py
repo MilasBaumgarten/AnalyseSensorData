616692a0d04f232ec4dc335a5b7e8d7610331031
Milas Baumgarten <milas.baumgarten@freenet.de>
2019-05-14 18:31:18 +0200

    synchronize data, improve code

--- AnalyseSensorData/Data.py
+++ AnalyseSensorData/Data.py
@@ -1,24 +1,63 @@
+from dataclasses import dataclass
 import numpy as np
 import sys
 
+@dataclass
+class Position:
+	x : float
+	y : float
+	z : float
+
+@dataclass
+class Rotation:
+	x : float
+	y : float
+	z : float
+	w : float
+
+@dataclass
+class Transform:
+	position : Position
+	rotation : Rotation
+
 class Data(object):
-	x_full = y_full = x = y_ecg = y_ecg_normalized = y_ecg_delta = y_eda = np.array([])
 	beat_delay = []
+	HMD = RightTracker = LeftTracker = []
+
+	HIGH = 0
+	LOW = 0
 
 	def __init__(self, tracking_data_file, sensor_data_file, min_delta, min_val, max_val, min_heart_beat_delay, max_heart_beat_delay, average_heart_beat_delay):
+		self.HIGH = max_val
+		self.LOW = min_val
+
 		# get raw data and separate into specific arrays
-		self.x_full, self.y_full = self.extract(tracking_data_file, sensor_data_file)
-		self.x = np.squeeze(np.asarray(self.y_full[:,np.array([True, False, False, False])]))
-		self.y_ecg = np.squeeze(np.asarray(self.y_full[:,np.array([False, False, True, False])]))
-		self.y_eda = np.squeeze(np.asarray(self.y_full[:,np.array([False, True, False, False])]))
+		tracking_full, sensor_full = self.extract(tracking_data_file, sensor_data_file)
+		self.sensor_x = np.squeeze(np.asarray(sensor_full[:,np.array([True, False, False, False])]))
+		self.ecg = np.squeeze(np.asarray(sensor_full[:,np.array([False, False, True, False])]))
+		self.eda = np.squeeze(np.asarray(sensor_full[:,np.array([False, True, False, False])]))
+		
+		# format tracking data
+		tracking_x =  []
+		for line in tracking_full:
+			tracking_x.append(line[0,0])
+			self.HMD.append(Transform(Position(line[0, 1], line[0, 2], line[0, 3]), Rotation(line[0, 4], line[0, 5], line[0, 6], line[0, 7])))
+			self.RightTracker.append(Transform(Position(line[0, 8], line[0, 9], line[0,10]), Rotation(line[0,11], line[0,12], line[0,13], line[0,14])))
+			self.LeftTracker.append(Transform(Position(line[0,15], line[0,16], line[0,17]), Rotation(line[0,18], line[0,19], line[0,20], line[0,21])))
+
+		self.tracking_x = np.array(tracking_x)
+		print(self.HMD[0].position.x)
 		
 		# calculate gradient and normalize ecg data
-		self.y_ecg_delta = self.calc_gradient(self.y_ecg, True)
-		self.y_ecg_normalized = self.normalize(self.y_ecg_delta, min_delta, min_val, max_val)
+		self.ecg_delta = self.calc_gradient(self.ecg, True)
+		self.ecg_normalized = self.normalize(self.ecg_delta, min_delta, min_val, max_val)
 
-		# try to fix measurment errors by inserting missed heartbeats and deleting duplicate heart beats
+		# try to fix measurment errors by inserting missed heartbeats and deleting
+		# duplicate heart beats
 		for i in range(0, 1):
-			self.calculate_missed_heart_beats(min_val, max_val, min_heart_beat_delay, max_heart_beat_delay, average_heart_beat_delay)
+			self.calculate_missed_heart_beats(self.LOW, self.HIGH, min_heart_beat_delay, max_heart_beat_delay, average_heart_beat_delay)
+
+		self.synchronize()
 
 	# Source: "The Best Way to Prepare a Dataset Easily" - Siraj Raval - https://www.youtube.com/watch?v=0xVqLJe9_CY&t=437s
 	def extract(self, tracking_filename, sensor_filename):
@@ -41,8 +80,9 @@ class Data(object):
 				line = line.replace("\t", " ")
 				line = line.replace(",", ".")
 				row = line.split(" ")
-				# add all relevant elements to the feature vectors (from a to (exclusive) b)
-				fvecs.append([float(x) for x in row[1:22]])
+				# add all relevant elements to the feature vectors (from a to (exclusive)
+				# b)
+				fvecs.append([float(x) for x in row[0:22]])
 			except:
 				print("Error with line: " + line)
 
@@ -51,6 +91,11 @@ class Data(object):
 			line = line.replace(";", "")
 			row = line.split(" ")
 			labels.append([float(x) for x in row[0:4]])
+
+		# remove time delay from sensor data
+		sensor_delay = labels[0][0]
+		for date in labels:
+			date[0] -= sensor_delay
 
 		# convert the features into a numpy float matrix
 		fvecs_np = np.matrix(fvecs).astype(np.float32)
@@ -90,24 +135,61 @@ class Data(object):
 	def calculate_missed_heart_beats(self, min_val, max_val, min_delay, max_delay, average_delay):
 		self.beat_delay.clear()
 		last = 0
-		for i in range(0, len(self.y_ecg_normalized)):
+		for i in range(0, len(self.ecg_normalized)):
 			# start check when a heart beat was found
-			if (self.y_ecg_normalized[i] == max_val):
+			if (self.ecg_normalized[i] == max_val):
 				index_delta = (i - last)
 				# remove a heart beat if it seems to be too fast aka duplicate
 				if (index_delta < min_delay):
 					# check which gradient was higher
-					if (self.y_ecg_delta[last] < self.y_ecg_delta[i]):
-						self.y_ecg_normalized[last] = min_val
+					if (self.ecg_delta[last] < self.ecg_delta[i]):
+						self.ecg_normalized[last] = min_val
 					else:
-						self.y_ecg_normalized[i] = min_val
+						self.ecg_normalized[i] = min_val
 				else :
 					# insert missed heart beats
 					if (index_delta > max_delay):
 						missed_beats = index_delta // average_delay
 						for j in range(1, missed_beats):
-							self.y_ecg_normalized[last + int(np.floor((j / missed_beats) * index_delta))] = max_val
+							self.ecg_normalized[last + int(np.floor((j / missed_beats) * index_delta))] = max_val
 							self.beat_delay.append(int(np.floor((1 / missed_beats) * index_delta)))
 					else:
 						self.beat_delay.append(index_delta)
 					last = i
+		return
+
+	# map sensor data onto tracking data
+	def synchronize(self):
+		ecg_synchronized = []
+		eda_synchronized = []
+		index_sensor = 0
+		high_recognized = False
+
+		for i in range (0, len(self.tracking_x) - 1):
+			# percentage = percentage at which point the next tracking date is in comparison to the current sensor date and the next one
+			percentage = 0
+			if ((percentage < 0.5 and self.ecg_normalized[index_sensor] == self.HIGH) or not (high_recognized)):
+				ecg_synchronized.append(self.HIGH)
+				high_recognized = True
+			else:
+				ecg_synchronized.append(self.LOW)
+
+			# sensor_data(tracking_index_x) = sensor_data(last_sensor_index) + percentage * sensor_date_delta_to_next_sensor_date
+			eda_synchronized.append(self.eda[index_sensor] + percentage * (self.eda[index_sensor + 1] - self.eda[index_sensor]))
+
+			while not (self.sensor_x[index_sensor + 1] > self.tracking_x[i + 1]):
+				if (self.ecg_normalized[index_sensor] == self.HIGH) and not (high_recognized):
+					high_recognized = False
+				index_sensor += 1
+
+		# add last data
+		if ((percentage < 0.5 and self.ecg_normalized[index_sensor] == self.HIGH) or not (high_recognized)):
+			ecg_synchronized.append(self.HIGH)
+		else:
+			ecg_synchronized.append(self.LOW)
+		eda_synchronized.append(self.eda[index_sensor] + percentage * (self.eda[index_sensor + 1] - self.eda[index_sensor]))
+
+		self.ecg_synchronized = np.array(ecg_synchronized)
+
+# TODO:
+#	synchronization: prioritise heart beats
--- AnalyseSensorData/TestStuff.py
+++ AnalyseSensorData/TestStuff.py
@@ -6,7 +6,7 @@ min_heart_beat_delay = 20
 max_heart_beat_delay = 35
 average_heart_beate_delay = 25
 min_normalized_value = 0
-max_normalized_value = 500
+max_normalized_value = 1
 min_beat_delta = 40
 
 data = Data( "TrackingData_2019-05-09-19-13-42.txt",
@@ -18,22 +18,44 @@ data = Data( "TrackingData_2019-05-09-19-13-42.txt",
 					max_heart_beat_delay,
 					average_heart_beate_delay)
 
-print(np.average(data.beat_delay))
-
-
 # combine arrays for the graph
 combined = []
-for i in range (0, len(data.y_ecg)):
-	combined.append([data.y_ecg[i], data.y_ecg_normalized[i]])
+print(len(data.ecg_synchronized))
+print(len(data.tracking_x))
+for i in range (0, len(data.tracking_x)):
+	combined.append([data.ecg_synchronized[i], data.HMD[i].position.x])
+
+combined_sensor = []
+for i in range (0, len(data.sensor_x)):
+	combined_sensor.append([data.ecg[i], data.ecg_normalized[i]])
+
+combined_HMD_pos = []
+for i in range (0, len(data.tracking_x)):
+	combined_HMD_pos.append([data.HMD[i].position.x, data.HMD[i].position.y, data.HMD[i].position.z])
+
+combined_HMD_rot = []
+for i in range (0, len(data.tracking_x)):
+	combined_HMD_rot.append([data.HMD[i].rotation.x, data.HMD[i].rotation.y, data.HMD[i].rotation.z, data.HMD[i].rotation.w])
+
+print(len(data.sensor_x), " - ", len(data.tracking_x))
 
 # draw graph
-plt.figure(1)
-plt.subplot(211)
-plt.ylabel("ecg")
-plt.plot(data.x, combined)
+plt.plot(data.tracking_x, combined)
+#plt.figure(1)
+#plt.subplot(221)
+#plt.ylabel("ecg")
+#plt.plot(data.sensor_x, combined_sensor)
 
-plt.subplot(212)
-plt.ylabel("delay")
-plt.plot(data.beat_delay)
+#plt.subplot(222)
+#plt.ylabel("delay")
+#plt.plot(data.beat_delay)
+
+#plt.subplot(223)
+#plt.ylabel("HMD Position")
+#plt.plot(data.tracking_x, combined_HMD_pos)
+
+#plt.subplot(224)
+#plt.ylabel("HMD Rotation")
+#plt.plot(data.tracking_x, combined_HMD_rot)
 
 plt.show()
